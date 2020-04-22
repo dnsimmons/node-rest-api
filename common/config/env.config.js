52624c45e88a7b123d3d432c6c82cb375c900b25
Dave Simmons <dsimmonsdesign@gmail.com>
2020-04-22 11:56:20 -0400

    Update env.config.js

--- common/config/env.config.js
+++ common/config/env.config.js
@@ -2,7 +2,7 @@ module.exports = {
     "port": 8000,
     "appEndpoint": "http://localhost:3600",
     "apiEndpoint": "http://localhost:3600",
-    "jwt_secret": "secret-phrase",
+    "jwt_secret": "your-secret-phrase",
     "jwt_expiration_in_seconds": 36000,
     "environment": "dev",
     "permissionLevels": {
@@ -10,5 +10,5 @@ module.exports = {
         "PERM_USER": 2,
         "PERM_ADMIN": 9999
     },
-    "mongo_str": "mongodb+srv://demo:fcaZ0KwomC7QziZE@cluster0-jmvfx.mongodb.net/node-rest-api?retryWrites=true&w=majority"
+    "mongo_str": "your-mongodb-connection-string"
 };
